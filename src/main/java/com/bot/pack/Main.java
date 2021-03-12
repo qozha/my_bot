55be84629197890fb3f66bacd464495bc913c934
qozha <madiabsadykov@gmail.com>
2021-03-12 23:46:32 +0600

    First commit

---
+++ src/main/java/com/bot/pack/Main.java
@@ -0,0 +1,18 @@
+package com.bot.pack;
+
+import org.springframework.boot.SpringApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotApplication.class, args);
+
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        } catch(TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/bot/pack/TelegramBotApplication.java
+++ src/main/java/com/bot/pack/TelegramBotApplication.java
@@ -2,14 +2,38 @@ package com.bot.pack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @SpringBootApplication
-public class TelegramBotApplication  extends TelegramLongPollingBot{
+public class TelegramBotApplication  extends TelegramLongPollingBot {
 
-	public String getBotUsername(){}
+	public String getBotUsername(){
 
-	public static void main(String[] args) {
-		SpringApplication.run(TelegramBotApplication.class, args);
+		return "Epic bot";
 	}
 
+
+
+	@Override
+	public String getBotToken() {
+		return null;
+	}
+
+	@Override
+	public void onUpdateReceived(Update update) {
+		if(update.hasMessage() && update.getMessage().hasText()){
+			SendMessage message = new SendMessage();
+			message.setChatId(String.valueOf(update.getMessage().getChatId()));
+			message.setText(update.getMessage().getText());
+
+			try{
+				execute(message);
+			} catch (TelegramApiException e){
+				e.printStackTrace();
+			}
+		}
+	}
 }
